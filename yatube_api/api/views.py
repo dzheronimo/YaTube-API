626e2699e6f37407a58791d8b9f263f5d88dd8e5
dzheronimo <dzheronimo@me.com>
2023-02-04 20:32:39 +0300

    Добавлены вьюсеты :    PostViewSet    CommentViewSet    GroupViewSet    FollowViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,55 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from rest_framework import filters
+
+from posts.models import Post, Group
+from .serializers import PostSerializer, CommentSerializer, GroupSerializer, FollowSerializer
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def get_post(self):
+        """Получает объкт модели Post"""
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        post = self.get_post()
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user,
+                        post=self.get_post())
+
+
+class GroupViewSet(ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    http_method_names = ['get', 'options', 'head']
+
+
+class FollowViewSet(ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return self.request.user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user
+        )
