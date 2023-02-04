318104737f283a8d6bea78442378837858c811f8
dzheronimo <dzheronimo@me.com>
2023-02-04 20:31:12 +0300

    Подключен DefaultRouter Прописаны роуты для эндпоинтов

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
+
+
+router = DefaultRouter()
+
+router.register(r'posts', PostViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+router.register(r'groups', GroupViewSet)
+router.register(r'follow', FollowViewSet, basename='follow')
 urlpatterns = [
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
